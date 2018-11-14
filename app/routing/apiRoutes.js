0194601ef1529b46af69e2d30cd7567f389a795c
Hamayon Hussain <hamayonhussain@Hamayons-MacBook-Pro.local>
2018-11-13 22:51:34 -0600

    file path fixed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,19 +2,19 @@
 var path = require('path');
 
 // Import the list of friend entries
-var friends = require('../data/friends');
+var friends = require('../data/friends.js');
 
 // Export API routes
 module.exports = function(app) {
 	// console.log('___ENTER apiRoutes.js___');
 
 	// Total list of friend entries
-	app.get('/app/data/friends.js', function(req, res) {
+	app.get('../data/friends.js', function(req, res) {
 		res.json(friends);
 	});
 
 	// Add new friend entry
-	app.post('/app/data/friends.js', function(req, res) {
+	app.post('../data/friends.js', function(req, res) {
 		// Capture the user input object
 		var userInput = req.body;
 		// console.log('userInput = ' + JSON.stringify(userInput));
